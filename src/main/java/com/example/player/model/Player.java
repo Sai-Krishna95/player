107713ba2dd807e7917d5b790c0ec6b9561e5857
saikrishna <saikrishna.skm1@gmail.com>
2024-08-26 08:37:09 +0000

    player committed

---
+++ src/main/java/com/example/player/model/Player.java
@@ -0,0 +1,73 @@
+/*
+ * You can use the following import statements
+ * import javax.persistence.*;
+ * 
+ */
+
+// Write your code here
+
+package com.example.player.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cricketteam")
+public class Player {
+
+    @Id
+    @Column(name = "playerId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int playerId;
+
+    @Column(name = "playerName")
+    private String playerName;
+
+    @Column(name = "jerseyNumber")
+    private int jerseyNumber;
+
+    @Column(name = "role")
+    private String role;
+
+    public Player() {
+
+    }
+
+    public Player(int playerId, String playerName, int jerseyNumber, String role) {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.jerseyNumber = jerseyNumber;
+        this.role = role;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(int jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
